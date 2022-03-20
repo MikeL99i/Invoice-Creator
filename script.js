c0a14b4a303aad1b5a64e5414774c47e2179b7b4
Mike Li <lim48687@gmail.com>
2022-03-20 11:32:00 +0100

    all services can only be added onece
    added alreadyAdd() function too prevent multiple same services

--- script.js
+++ script.js
@@ -5,6 +5,9 @@ const mowBtn = document.getElementById("mowbutton");
 const pullBtn = document.getElementById("pullbutton");
 const invoiceBtn = document.getElementById("invoicebtn");
 
+// remove buttons
+const removeWash = document.getElementById("removewash"); 
+
 // used for adding new html (innerhtml) to display changes
 const midPart = document.getElementById("midpart");
 const botPart = document.getElementById("botpart");
@@ -17,24 +20,34 @@ let requestedServices = [null, null, null];
 // variable for total cost
 let costSum = 0;
 
+// flag for already added services
+let washAdd = 0;
+let mowAdd = 0;
+let pullAdd = 0;
+
 
 // EventListener instead of onclick 
 washBtn.addEventListener("click", function() {
     requestedServices[0] = "WASH";
     renderMid(requestedServices);
-    renderBot(10);
+    alreadyAdd("WASH");
 })
 
 mowBtn.addEventListener("click", function() {
     requestedServices[1] = "MOW";
     renderMid(requestedServices);
-    renderBot(20);
+    alreadyAdd("MOW");
 })
 
 pullBtn.addEventListener("click", function() {
     requestedServices[2] = "PULL";
     renderMid(requestedServices);
-    renderBot(30);
+    alreadyAdd("PULL");
+})
+
+// remove Eventlistner
+removeWash.addEventListener("click", function() {
+    updateMid.innerHTML = "";
 })
 
 // rendering midPart changes
@@ -43,7 +56,7 @@ function renderMid(arr) {
     if(arr[0] === "WASH") {
         render += `
         <p>
-            <span class="leftpara">Wash Car</span>
+            <span class="leftpara">Wash Car <button class="removebtn" id="removewash">remove</button></span>
             <span class="rightpara">10 $</span>
         </p>
         ` 
@@ -51,7 +64,7 @@ function renderMid(arr) {
     if(arr[1] === "MOW") {
         render += `
         <p>
-            <span class="leftpara">Mow Lawn</span>
+            <span class="leftpara">Mow Lawn <button class="removebtn" id="removemow">remove</button></span>
             <span class="rightpara">20 $</span>
         </p>
         ` 
@@ -60,7 +73,7 @@ function renderMid(arr) {
     if(arr[2] === "PULL") {
         render += `
         <p>
-            <span class="leftpara">Pull Weeds</span>
+            <span class="leftpara">Pull Weeds <button class="removebtn" id="removepull">remove</button></span>
             <span class="rightpara">30 $</span>
         </p>
         ` 
@@ -75,4 +88,29 @@ function renderBot(cost) {
     costSum += cost;
     render = `<p>${costSum} $</p>`;
     totalCost.innerHTML = render;
+}
+
+// check if service is already added
+function alreadyAdd(service) {
+    if(service === "WASH") {
+        if(washAdd === 0) {
+            renderBot(10);
+            washAdd = 1;
+        }else 
+            alert("Already Added!");
+    }
+    if(service === "MOW") {
+        if(mowAdd === 0) {
+            renderBot(20);
+            mowAdd = 1;
+        }else
+            alert("Already Added!");
+    }
+    if(service === "PULL") {
+        if(pullAdd === 0) {
+            renderBot(30);
+            pullAdd = 1;
+        }else
+            alert("Already Added!");
+    }
 }
